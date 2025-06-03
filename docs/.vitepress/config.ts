7affe2aab55b54081e57fabbabc1815bde1c3817
Turiyatree Technologies <150682480+t3-team@users.noreply.github.com>
2025-06-03 05:13:15 +0000

    config.ts upd1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       {
         text: 'Getting Started',
         items: [
-          { text: 'Introduction', link: '/guide/' }
+          { text: 'Introduction', link: '/guide/' },
           { text: 'Releases', link: '/changelog/' }
         ]
       },
