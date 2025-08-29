6fa38ebb7a2078f044fcb06d267ad30a77510b9f
Turiyatree Technologies <150682480+t3-team@users.noreply.github.com>
2025-08-29 15:48:46 +0000

    config ts update for navigate features

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
         text: 'Getting Started',
         items: [
           { text: 'Introduction', link: '/guide/' },
+          { text: 'Navigate Features', link: '/guide/navigate-features' },
           { text: 'Releases', link: '/changelog/' }
         ]
       },
@@ -45,7 +46,6 @@ export default defineConfig({
           { text: 'How to do JD, Resume Parsing?', link: '/features/jd-resume-parsing' },
           { text: 'How to Generate Channel Analysis Report', link: '/guide/channel-analysis' },
           { text: 'How to Install Safe Exam Browser', link: '/guide/safe-exam-browser' },
-          { text: 'How to Navigate Turiyaskills Features', link: '/guide/navigate-features' },
           { text: 'How to use the features for Small Biz?', link: 'features/small-business'},
           { text: 'How to create skill assessments from JD?', link: 'features/workflow'},
           { text: 'How to use ContactOut API Key?', link: '/features/contactout-integration'},
